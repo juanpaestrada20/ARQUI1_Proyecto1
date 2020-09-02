569fe17d13bc12323425dee543b4cde3db529d73
Jorge Canté <jlutin.coke@gmail.com>
2020-09-01 20:55:39 -0600

    Teclado integrao

--- Proyecto/Proyecto.ino
+++ Proyecto/Proyecto.ino
@@ -1,9 +1,10 @@
 #include <LiquidCrystal.h>
+#include <Adafruit_Keypad.h>
 
 /*
     The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
+ * LCD RS pin to digital pin 7
+ * LCD Enable pin to digital pin 6
  * LCD D4 pin to digital pin 5
  * LCD D5 pin to digital pin 4
  * LCD D6 pin to digital pin 3
@@ -15,22 +16,44 @@
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
 */
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
+//Colocamos todo lo necesario para el teclado
+char keys[4][3] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte rowPins[4] = {25, 26, 27, 28}; //connect to the row pinouts of the keypad
+byte colPins[3] = {22, 23, 24}; //connect to the column pinouts of the keypad
+
+Adafruit_Keypad teclado = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, 4, 3);
+
 void setup() {
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
+  lcd.begin(20, 2);
   // Print a message to the LCD.
-  lcd.print("23sex!");
+  lcd.print("Bienvenido");
 
+  //Teclado de empieza xd
+  teclado.begin();
+
+  //Nos posicionamos en la columna 0 y fila 1
+  lcd.setCursor(0, 1);
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 236);
+  teclado.tick();
+
+  while(teclado.available()){
+    keypadEvent e = teclado.read();
+    if (e.bit.EVENT == KEY_JUST_PRESSED) lcd.print((char)e.bit.KEY);
+  }
+
+  
+
 }
